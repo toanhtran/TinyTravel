378067d021baf1a5d5fbb6e2aabbceb122ba8e93
toanhtran <toanh.t.tran@gmail.com>
2016-11-19 21:08:20 -0800

    add location class

--- main/views.py
+++ main/views.py
@@ -5,8 +5,19 @@ from django.http import HttpResponse
 # def home(request):
 #     return HttpResponse('<h1>Hello Tiny Travelers!</h1>')
 def home(request):
-    name = "Phillip Kaggie"
-    age = "0-12 months"
-    context = {'tiny_traveler_name': name,
-                'tiny_traveler_age': age }
+    context = {'locations':locations }
     return render(request, 'index.html', context)
+
+class Location:
+    def __init__(self, city, state, img_url):
+        self.city = city
+        self.state = state
+        self.img_url = img_url
+
+locations = [
+    Location("Fool's Falls", "CO",
+             'http://courseware.codeschool.com/try_django/images/fools-falls.jpg'),
+    Location("Curly's Creek", "NM",
+             'http://courseware.codeschool.com/try_django/images/curlys-creek.jpg'),
+    Location("The Delicate Arch", "UT", 'http://courseware.codeschool.com/try_django/images/delicate-arch.jpg')
+]
