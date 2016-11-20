12655abc6e10bf3459d90d3e31d487eff9506f26
toanhtran <toanh.t.tran@gmail.com>
2016-11-20 08:52:14 -0800

    location model

---
+++ main/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('city', models.CharField(max_length=100)),
+                ('state', models.CharField(max_length=100)),
+                ('img_url', models.CharField(max_length=100)),
+            ],
+        ),
+    ]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    img_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
